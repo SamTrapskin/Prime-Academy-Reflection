447cd9e0caa1252958c8e4e5d743ca1020370cea
Sam Trapskin <samtrapskin@gmail.com>
2018-04-20 15:25:06 -0500

    initial - base project commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Oh, the places you've been</h1>
-          <h4><i>Reflection Board</i></h4>
+          <h4><i>Prime Digital Academy Reflection</i></h4>
         </header>
         <br/>
       </div>
